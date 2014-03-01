4e638f9f255fd75977850a9dde53b878436c2036
vikassy <vikasyaligar.it@gmail.com>
2014-03-01 19:42:53 +0530

    Done with style

--- backend/map_my_village/app/controllers/location_controller.rb
+++ backend/map_my_village/app/controllers/location_controller.rb
@@ -1,8 +1,10 @@
 class LocationController < ApplicationController
   def create
   	# @location = Location.new
-  	data = JSON.parse(params[:data])
-  	Location.create(project_id: data["project_id"],latitude: data["lat"],longitude: data["long"])
+  	# data = JSON.parse(params[:data])
+  	# Location.create(project_id: data["project_id"],latitude: data["lat"],longitude: data["long"])
+  	# render json: {status: true} 
+  	Location.create(project_id: params["project_id"],latitude: params["latitude"],longitude: params["longitude"])
   	render json: {status: true} 
   end
 end
